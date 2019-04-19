ce60f63c530b9b1d2c8ffa2f029e330c709741bc
SkyChaseHsu <buhruw-3gyzpU-jykbaw>
2019-04-19 19:57:03 +0800

    Comments & Note

--- rename.py
+++ rename.py
@@ -7,20 +7,26 @@ def rename():
 
     filelist = os.listdir(path)
     for files in filelist:
+        # file's old location
     	old_dir = os.path.join(path, files)
+        # Split the file name and type
     	filename = os.path.splitext(files)[0]
     	filetype = os.path.splitext(files)[1]
+
+        # Pass other files
     	if filetype != '.c':
     		continue
     	# change from PAT-1001 to PAT_1001
         # - We cannot change the char in the string directly
-        # - So we turn it in the list, change the  element in the list,
+        # - So we turn it in a list, in which we change the certain element
         # - and them turn it into string
     	fn_list = list(filename)
         fn_list[3] = '_'
         new_filename = ''.join(fn_list)
 
+        # file's new location
     	new_dir = os.path.join(path, new_filename + filetype)
+
     	os.rename(old_dir, new_dir)
 
 rename()
