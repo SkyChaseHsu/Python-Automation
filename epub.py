678d33ddb0783d412b097dbd48e05fca6700dc86
SkyChaseHsu <buhruw-3gyzpU-jykbaw>
2019-04-20 14:43:25 +0800

    epub.py:change the second part of cpt7

--- epub.py
+++ epub.py
@@ -2,7 +2,7 @@ import os
 
 def add_tit_no (filename, cpt_no):
 	after_sec = 0
-	sec_no = 1
+	sec_no = 8
 	with open(filename) as read_f,open('temp.swap','w') as write_f:
 		for line in read_f:
 			if line.find('section') > 0:
@@ -17,14 +17,5 @@ def add_tit_no (filename, cpt_no):
 	os.remove(filename)
 	os.rename('temp.swap', filename)
 
-add_tit_no('ch01-introduction.xhtml', 	1)
-add_tit_no('ch02-git-basics.xhtml', 	2)
-add_tit_no('ch03-git-branching.xhtml', 	3)
-add_tit_no('ch04-git-server.xhtml', 	4)
-add_tit_no('ch05-distributed-git.xhtml', 5)
-add_tit_no('ch06-github.xhtml', 		6)
-add_tit_no('ch07-git-tools_split_000.xhtml', 7)
-add_tit_no('ch08-customizing-git.xhtml', 8)
-add_tit_no('ch09-git-and-other-scms.xhtml', 9)
-add_tit_no('ch10-git-internals.xhtml', 10)
+add_tit_no('ch07-git-tools_split_001.xhtml', 	7)
 
