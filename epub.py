6c8cd308f3bc7b7df2e37ec3e618902d976b588a
SkyChaseHsu <buhruw-3gyzpU-jykbaw>
2019-04-20 14:21:39 +0800

    epub.py:change chapter 1-10

--- epub.py
+++ epub.py
@@ -17,5 +17,14 @@ def add_tit_no (filename, cpt_no):
 	os.remove(filename)
 	os.rename('temp.swap', filename)
 
-add_tit_no('ch03-git-branching.xhtml',3)
+add_tit_no('ch01-introduction.xhtml', 	1)
+add_tit_no('ch02-git-basics.xhtml', 	2)
+add_tit_no('ch03-git-branching.xhtml', 	3)
+add_tit_no('ch04-git-server.xhtml', 	4)
+add_tit_no('ch05-distributed-git.xhtml', 5)
+add_tit_no('ch06-github.xhtml', 		6)
+add_tit_no('ch07-git-tools_split_000.xhtml', 7)
+add_tit_no('ch08-customizing-git.xhtml', 8)
+add_tit_no('ch09-git-and-other-scms.xhtml', 9)
+add_tit_no('ch10-git-internals.xhtml', 10)
 
