6c1f13629a8e6b913fe0ed3eb00a8186bcea3316
SkyChaseHsu <buhruw-3gyzpU-jykbaw>
2019-04-20 13:52:54 +0800

    make a function: change the chapter no

--- epub.py
+++ epub.py
@@ -1,21 +1,21 @@
 import os
 
-after_sec = 0
-tit_no = 8
- 
-with open('ch03-git-branching.xhtml') as read_f,open('.ch03-git-branching.xhtml.swap','w') as write_f:
-    for line in read_f:
-    	if line.find('section') > 0:
-    		after_sec = 1
-    		continue
-    	if after_sec == 1 and line.find('id') > 0 and line.find('h2') > 0:
-    		line = line.replace('">8', '">')
-    		after_sec = 0
-    	write_f.write(line)
-    		
-        # line=line.replace('SB','h2')
-        # write_f.write(line)
- 
-os.remove('ch03-git-branching.xhtml')
-os.rename('.ch03-git-branching.xhtml.swap','ch03-git-branching.xhtml')
+def add_tit_no (filename, cpt_no):
+	after_sec = 0
+	sec_no = 1
+	with open(filename) as read_f,open('temp.swap','w') as write_f:
+		for line in read_f:
+			if line.find('section') > 0:
+				after_sec = 1
+				continue
+			if after_sec == 1 and line.find('id') > 0 and line.find('h2') > 0:
+				line = line.replace('">', '">' + str(cpt_no) + '.' + str(sec_no) + ' ')
+				sec_no += 1
+				after_sec = 0
+			write_f.write(line)
+
+	os.remove(filename)
+	os.rename('temp.swap', filename)
+
+add_tit_no('ch03-git-branching.xhtml',3)
 
